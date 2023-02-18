2b13780a5912748ae77e61cbfca2a60716091080
nflores97 <106122495+nflores97@users.noreply.github.com>
2023-02-18 00:44:57 -0800

    new input
    each new input gets this message stored in json file
    Yooooooo whts uppppppp? Lets partyyyyyyyyyyy!

--- open_calls/twillio_webhook.py
+++ open_calls/twillio_webhook.py
@@ -46,7 +46,7 @@ def handle_request():
     if sent_input in CORPUS['input']:
         response = random.choice(CORPUS['input'][sent_input])
     else:
-        CORPUS['input'][sent_input] = ['DID NOT FIND']
+        CORPUS['input'][sent_input] = ['Yooooooo whts uppppppp? Lets partyyyyyyyyyyy!']
         with open('chatbot_corpus.json', 'w') as myfile:
             myfile.write(json.dumps(CORPUS, indent=4 ))
 
